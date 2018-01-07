aeeadac58317b9182c84f951ed1ddbc7880750a3
zahrank21 <zahrank@gmail.com>
2018-01-07 09:36:08 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -97,7 +97,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
